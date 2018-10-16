28303f9a9d1d375a10921b6390199906bc8fad4c
Bartek <bpkonieczny@gmail.com>
2018-10-16 22:17:53 +0200

    fixing https bug by workaround

--- server/src/response/responseManager.js
+++ server/src/response/responseManager.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const https = require('https');
+
 const responseApiUrl = 'https://35.178.111.80/question';
 
 class ResponseManager {
@@ -6,7 +8,9 @@ class ResponseManager {
 
   getResponse(ques) {
     let requestBody = {question: ques};
-    return axios.post(responseApiUrl, requestBody);
+    return axios.post(responseApiUrl, requestBody, {
+        httpsAgent: new https.Agent({rejectUnauthorized: false})
+    });
   }
 }
 
