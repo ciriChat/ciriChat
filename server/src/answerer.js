722a7b05b805445b400ff9b566f6549baee7b0a3
barkon <bpkonieczny@gmail.com>
2018-12-11 22:14:50 +0100

    added response api url as environmental variable

--- server/src/answerer.js
+++ server/src/answerer.js
@@ -1,21 +1,8 @@
 const axios = require('axios');
-const answerApiUrl = "http://35.178.11.80/";//  /?question=
+const responseApiUrl = process.env.RESPONSE_API_URL || "https://35.195.184.216/question?question=";
 
 export function getAnswer(question) {
-
-  let answer = axios.post(answerApiUrl, {
-    question: question
-  })
-  .then(function (response) {
-    let bestIndex = response.best_index;
-    let bestAnswer = response.results[bestIndex]; // object: {answer: "something", score: 0}
-    console.log(`Best index: ${best_index}, best answer: ${bestAnswer.answer}`);
-    return best_answer;
-  })
-  .catch(function (error) {
-    console.log(`Error: ${error}`);
-  });
-  console.log(`Question: ${question}, answer: ${answer}`);
-  return answer.answer;
+  let url = responseApiUrl + question;
+  console.log(`Sending get request to url ${url}`);
+  return axios.get(question);
 }
-
--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const answerer = require('answerer');
+const answerer = require('./answerer.js');
 
 const app = express();
 app.use(cors());
 
-app.listen(app.get('port'), () => console.log("Node app is running at localhost:" + app.get('port')));
+const port = process.env.PORT || 1337;
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.post('/question', function (request, response) {
-  let answer = answerer.getAnswer(request.question);
-  console.log(`Received answer from api: ${answer}`);
-  response.status(200).send(answer);
+
+app.post('/question', function (req, res) { 
+  let question = req.body.question;
+  console.log(`Received question: '${question}'`)
+  // body is {question: "some question"}
+  answerer.getAnswer(question) 
+  .then(function (response) {
+    console.log(`Received response with code: ${response.status}`)
+    
+    let bestIndex = response.data.best_index;
+    let bestAnswer = response.data.results[bestIndex]; // object: {answer: "something", score: 0}
+    console.log(`Best index: ${best_index}, best answer: ${bestAnswer.answer}`);
+    let answer = { 
+      question: question,
+      answer: bestAnswer.answer
+    };
+    res.status(200).send(answer);
+  })
+  .catch(function (error) {
+    console.log(`Error: ${error}`);
+  });
 });
